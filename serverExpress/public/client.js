0dda57156b9e266ab5314e880741a3f434e43b65
Agustin <taibocruz@gmail.com>
2022-04-24 00:03:24 -0300

    Entregable clase 12 y primeros pasos de la primer entrega del finak

--- serverExpress/public/client.js
+++ serverExpress/public/client.js
@@ -1,27 +1,88 @@
 const socket = io.connect();
 
 const form = document.getElementById('form');
-console.log(form);
+// console.log(form);
 
 
 const author = document.getElementById('username');
 const text = document.getElementById('texto');
 
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
 
-    const mensaje = {
-        nombre: author.value,
-        mensaje: text.value
-    };
+//     const mensaje = {
+//         nombre: author.value,
+//         mensaje: text.value
+//     };
 
-    author.value = '';
-    text.value = '';
+//     author.value = '';
+//     text.value = '';
 
-    socket.emit('nuevo-mensaje', mensaje);
+//     socket.emit('nuevo-mensaje', mensaje);
 
 
 
-    console.log(mensaje);
-});
+//     console.log(mensaje);
+// });
+
+socket.on('productCatalog', (productsData) => renderProducts(productsData));
+let renderProducts = (productsData) => {
+    if (productsData.products.length > 0) {
+        //Productos
+        let htmlProductos = productsData.products.map(e => `
+        <div class="row product">
+            <div class="col">${e.title}</div>
+            <div class="col">$ ${e.price}</div>
+        </div>`
+        ).join(' ');
+
+        //document.getElementById('viewTitle').innerHTML = productsData.viewTitle;
+        document.getElementById('productCatalog').innerHTML = htmlProductos;
+    } else {
+        let html = `<div class="error" style="padding:2em;text-align:center">${data.errorMessage}</div>`;
+        document.getElementById('productCatalog').innerHTML = html;
+    }
+}
+
+function createProd(form) {
+    console.log("Nuevo producto agregado!");
+    let newProduct = {
+        title: document.getElementById('title').value,
+        price: parseFloat(document.getElementById('price').value),
+    }
+    socket.emit('newProduct', newProduct)
+    console.log(newProduct);
+    return false;
+}
+
+socket.on('messages', (messagesData) => renderMessages(messagesData));
+let renderMessages = (messagesData) => {
+    console.log('client :', messagesData);
+    //Mensajes
+    let htmlMensajes = messagesData.messages.map((e, i) => `
+            <div class="row">
+                <strong style="color: blue; font-size: 20px">${e.email} <em style="color: black; font-size: 12px">${e.date}</em></strong>
+                <em style="color: black; font-size: 20px; padding:0.5em">- ${e.msg}</em>
+            </div>`
+    ).join(' ');
+    document.getElementById('messages').innerHTML = htmlMensajes;
+}
+
+function getFormattedDate() {
+    var date = new Date();
+    var str = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    return str;
+}
+
+function sendMsg(form) {
+    let fullMsg = {
+        email: document.getElementById('email').value,
+        date: getFormattedDate(),
+        msg: document.getElementById('msg').value
+    }
+    socket.emit('newMsg', fullMsg)
+    console.log(fullMsg);
+    return false;
+}
--- serverExpress/src/index.js
+++ serverExpress/src/index.js
@@ -4,7 +4,7 @@ const io = require('socket.io');
 const mainRouter = require('../routes/mainRouter');
 const path = require('path');
 const {controller} = require('../src/controller/productosController');
-
+const fs = require('fs');
 //Inicializacion de express
 const app = express();
 const PORT = 8080;
@@ -30,27 +30,81 @@ app.get('/', (req, res) => {
     res.render('index', { productos });
 });
 
+let messagesFile = 'src/mensajes/mensajes.json';
+
+//Funciones para persistencia de datos en mensajes.json
+function leerMensajes() {
+    let messages = fs.readFileSync(messagesFile, "utf-8")
+    let parsedMessages = JSON.parse(messages);
+    console.log("File read correctly.");
+    return parsedMessages;
+}
+
+function guardarMensajes(msj) {
+    let messages = leerMensajes();
+    messages.push(msj);
+    fs.writeFileSync(messagesFile, JSON.stringify(messages));
+    console.log("Message saved.");
+}
+
+let productCatalog = [{
+    id: 1,
+    title: "Nintendo Switch OLED",
+    price: 92999.99,
+    thumbnail: "https://assets.nintendo.com/image/upload/b_white,c_pad,f_auto,h_382,q_auto,w_573/ncom/en_US/switch/site-design-update/hardware/switch/nintendo-switch-oled-model-white-set/gallery/image01?v=2022021721"
+},
+{
+    id: 2,
+    title: "PlayStation 5",
+    price: 198999.99,
+    thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvp64pH79Rc064zruKT4qtKKEfN8NbMWhXnEd6Htxq5S06yvoyzWZq4rmm_Swr_dM8Z80&usqp=CAU"
+},
+{
+    id: 3,
+    title: "Mario Kart 8 Deluxe",
+    price: 8999.99,
+    thumbnail: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_280x437/public/media/image/2017/02/mario-kart-8-deluxe-caratula.jpg?itok=XuGvkgAx"
+}
+];
+
+
+
+
 const WSServer = io(myServer);
 
 WSServer.on('connection', (socket) => {
     console.log('Nueva conexion');
-    // socket.on('disconnect', () => {
-    //     console.log('Conexion terminada');
-    // });
-    // socket.on('nuevo-producto', (producto) => {
-    //     controller.add(producto);
-    //     WSServer.emit('nuevo-producto', producto);
-    // });
-    // socket.on('borrar-producto', (id) => {
-    //     controller.delete(id);
-    //     WSServer.emit('borrar-producto', id);
-    // });
-    // socket.on('editar-producto', (producto) => {
-    //     controller.edit(producto);
-    //     WSServer.emit('editar-producto', producto);
-    // });
+    console.log(socket.id);
+    console.log(socket.client.id);
+
+    socket.emit('productCatalog', {
+        products: productCatalog,
+    });
+    socket.on('newProduct', (data) => {
+        console.log('esta es la data q llega:',data);
+        productCatalog.push({
+            id: productCatalog.length + 1,
+            ...data
+        });
+        console.log(productCatalog);
+        io.sockets.emit('productCatalog', {
+            products: productCatalog
+        });
+    });
+
     socket.on('nuevo-mensaje', (mensaje) => {
-        console.log(`Nuevo mensaje de: ${mensaje.nombre} \n dice: ${mensaje.mensaje}`);
+        console.log(`Nuevo mensaje de: ${mensaje.nombre}\n dice: ${mensaje.mensaje}`);
+    });
+
+    let messages = leerMensajes();
+    socket.emit('messages', {
+        messages: messages
+    });
+    socket.on('newMsg', (data) => {
+        guardarMensajes(data);
+        io.sockets.emit('messages', {
+            messages: leerMensajes()
+        });
     });
 
 })
